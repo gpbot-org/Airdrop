a2dad310701f0c7513a8f1c84c4afa7285ecad8f
gpbot-org <fakeif352@gmail.com>
2024-10-13 12:07:55 -0700

    Update fire.py

--- fire.py
+++ fire.py
@@ -6,13 +6,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 firebase_config = {
-    "apiKey": os.getenv("API_KEY"),
-    "authDomain": os.getenv("AUTH_DOMAIN"),
-    "databaseURL": os.getenv("DATABASE_URL"),
-    "projectId": os.getenv("PROJECT_ID"),
-    "storageBucket": os.getenv("STORAGE_BUCKET"),
-    "messagingSenderId": os.getenv("MESSAGING_SENDER_ID"),
-    "appId": os.getenv("APP_ID")
+    "apiKey": os.getenv("FIREBASE_API_KEY"),
+    "authDomain": os.getenv("FIREBASE_AUTH_DOMAIN"),
+    "databaseURL": os.getenv("FIREBASE_DATABASE_URL"),
+    "projectId": os.getenv("FIREBASE_PROJECT_ID"),
+    "storageBucket": os.getenv("FIREBASE_STORAGE_BUCKET"),
+    "messagingSenderId": os.getenv("FIREBASE_MESSAGING_SENDER_ID"),
+    "appId": os.getenv("FIREBASE_APP_ID")
 }
 
 firebase = pyrebase.initialize_app(firebase_config)
